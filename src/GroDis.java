02da187568d72399e9c02a589613a9753e996a85
O'Cyrus JB <s1291542@monmouth.edu>
2020-02-06 20:42:39 -0500

    grocery

--- src/GroDis.java
+++ src/GroDis.java
@@ -8,11 +8,14 @@ private static float costP;
 	{
 		do {
 		Scanner input = new Scanner (System.in);
+		System.out.println("Please not to make sure your input is a number and not a letter character "
+				+ "or else the program will not recognize your response. ");
+		System.out.println("Make sure the cost is less than 1000 and not a negative number");
 		
-		System.out.println("Enter the cost of your groceries or 9 to end program: ");
+		System.out.println("Enter the cost of your groceries or 0 to end program: ");
 		cost = input.nextFloat();
 	
-	    if (cost < 0 && cost < 10)
+	    if (cost >= 1 && cost < 10)
 		{
 			System.out.println("No coupon");
 		}
@@ -20,32 +23,37 @@ private static float costP;
 		else if (cost >= 10 && cost < 60)
 		{
 			costP = (float) (cost*.08);
-			System.out.println("You win a discount coupon of $"+costP+"( 8% of your purchase");
+			System.out.println("You win a discount coupon of $"+costP+" ( 8% of your purchase");
 		}
 	    
 		else if (cost >= 60 && cost < 150)
 		{
 			costP = (float) (cost*.10);
-			System.out.println("You win a discount coupon of $"+costP+"( 10% of your purchase");
+			System.out.println("You win a discount coupon of $"+costP+" ( 10% of your purchase");
 		}
 		
 		else if (cost >= 150 && cost < 210)
 		{
 			costP = (float) (cost*.08);
-			System.out.println("You win a discount coupon of $"+costP+"( 12% of your purchase");
+			System.out.println("You win a discount coupon of $"+costP+" ( 12% of your purchase");
 		}
 		
-		else if (cost > 210)
+		else if (cost > 210 && cost <= 1000)
 		{
 			costP = (float) (cost*.14);
-			System.out.println("You win a discount coupon of $"+costP+"( 14% of your purchase");
+			System.out.println("You win a discount coupon of $"+costP+" ( 14% of your purchase");
 		}
 	    
 		else if (cost == 0)
 		{
 			System.out.println("End Program");
 		}
-	} while (cost > 0);
+	    
+		else if (cost <= -1 && cost > 1000)
+		{
+			System.out.print("Error! please enter a response between 0 and 1000 dollars. ");
+		}
+	} while (cost != 0);
 	
 	}
 
