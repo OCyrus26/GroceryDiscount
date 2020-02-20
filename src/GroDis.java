3feff8e9fede957d18867761a08869cb583fe039
O'Cyrus JB <s1291542@monmouth.edu>
2020-02-20 13:35:53 -0500

    Correct version 1

--- src/GroDis.java
+++ src/GroDis.java
@@ -6,33 +6,38 @@ private static float costP;
 
 	public static void main(String[] args) 
 	{
+		//do while loop to run the program until user inputs 0
 		do {
+			
+		//Scanner
 		Scanner input = new Scanner (System.in);
-		System.out.println("Please not to make sure your input is a number and not a letter character "
-				+ "or else the program will not recognize your response. ");
-		System.out.println("Make sure the cost is less than 1000 and not a negative number");
+		
+		//prints instructions 
+		System.out.println("Make sure your cost is a number");
+		System.out.println("Make sure the cost is not greater than 1000 and not less than 0");
 		
 		System.out.println("Enter the cost of your groceries or 0 to end program: ");
 		cost = input.nextFloat();
-	
+		
+		//Determines your coupon based on cost 
 	    if (cost >= 1 && cost < 10)
 		{
 			System.out.println("No coupon");
 		}
 		
-		else if (cost >= 10 && cost < 60)
+		else if (cost >= 10 && cost <= 60)
 		{
 			costP = (float) (cost*.08);
 			System.out.println("You win a discount coupon of $"+costP+" ( 8% of your purchase");
 		}
 	    
-		else if (cost >= 60 && cost < 150)
+		else if (cost > 60 && cost <= 150)
 		{
 			costP = (float) (cost*.10);
 			System.out.println("You win a discount coupon of $"+costP+" ( 10% of your purchase");
 		}
 		
-		else if (cost >= 150 && cost < 210)
+		else if (cost > 150 && cost <= 210)
 		{
 			costP = (float) (cost*.08);
 			System.out.println("You win a discount coupon of $"+costP+" ( 12% of your purchase");
@@ -46,12 +51,12 @@ private static float costP;
 	    
 		else if (cost == 0)
 		{
-			System.out.println("End Program");
+			System.out.println("End Program"); //Ends program 
 		}
 	    
-		else if (cost <= -1 && cost > 1000)
+		else if (cost < 0 && cost > 1000)
 		{
-			System.out.print("Error! please enter a response between 0 and 1000 dollars. ");
+			
 		}
 	} while (cost != 0);
 	
